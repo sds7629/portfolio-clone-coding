f28d1589576bd63f0d60b3c43b9f9e9986b682d6
sds7629 <sds7629@naver.com>
2022-12-26 16:46:56 +0900

    JS:Navbar meu click

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+
+//navbar clicked move
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener('click',(event)=>{
+
+    const targetTo = event.target;
+    const linkTo = targetTo.dataset.link;
+
+    if(linkTo == null){
+        return;
+    }
+    
+    const scrollTo = document.querySelector(linkTo);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+});
+
+
